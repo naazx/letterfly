e67ffd0992191169667c04fe1548461bc9465c71
Nazar Dydyn <_email@gmail.com>
2026-06-27 14:04:39 +0300

    Initial Commit

---
+++ PawLetter/PawLetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawLetterApp.swift
+//  PawLetter
+//
+//  Created by Nazar Dydyn on 27.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct PawLetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PawLetterTests/PawLetterTests.swift
@@ -0,0 +1,17 @@
+//
+//  PawLetterTests.swift
+//  PawLetterTests
+//
+//  Created by Nazar Dydyn on 27.06.2026.
+//
+
+import Testing
+@testable import PawLetter
+
+struct PawLetterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
