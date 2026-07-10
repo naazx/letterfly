f0b15886b6c9256aba1e11204c6572ab8fd609a7
Nazar Dydyn <nazardydyn83@gmail.com>
2026-07-10 21:25:02 +0300

    ProfileView: показ inviteCode, аватарка-заглушка (SF Symbol)

--- PawLetter/Profile/ProfileView.swift
+++ PawLetter/Profile/ProfileView.swift
@@ -5,15 +5,49 @@
 //  Created by Nazar Dydyn on 10.07.2026.
 //
 
+import FirebaseAuth
 import SwiftUI
 
 struct ProfileView: View {
+    @State private var inviteCode: String?
+    
     var viewModel: AuthViewModel
+    var userServices = UserServices()
+    
     var body: some View {
-            Button("Logout", systemImage: "person.crop.circle.fill.badge.xmark") {
-                viewModel.signOut()
+        NavigationStack{
+                Form {
+                    Section{
+                        Image(systemName: "person.circle.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                            .frame(maxWidth: .infinity, alignment: .center)
+                            .foregroundStyle(.blue)
+                    }
+                    .listRowBackground(Color.clear)
+                    Section("Your code"){
+                        Text(inviteCode ?? "Loading...")
+                    }
+                    Button("Logout", systemImage: "person.crop.circle.fill.badge.xmark") {
+                        viewModel.signOut()
+                    }
+                }
+                .task {
+                    guard let user = Auth.auth().currentUser else {
+                        inviteCode = "NO USER"
+                        return
+                    }
+                    do {
+                        let fetchedCode = try await userServices.fetchInviteCode(uid: user.uid)
+                        inviteCode = fetchedCode ?? "NIL CODE"
+                    } catch {
+                        inviteCode = "ERROR: \(error.localizedDescription)"
+                    }
+                }
+                .navigationTitle("Profile")
             }
-    }
+        }
 }
 
 #Preview {
