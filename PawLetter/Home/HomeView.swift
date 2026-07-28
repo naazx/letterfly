75ae60a9d2583311f6874b4b4c99f603ab56a30a
Nazar Dydyn <nazardydyn83@gmail.com>
2026-07-28 19:20:58 +0300

    add dark icon, bug fix

--- PawLetter/Home/HomeView.swift
+++ PawLetter/Home/HomeView.swift
@@ -16,56 +16,58 @@ struct HomeView: View {
     
     var body: some View {
         NavigationStack {
-            if homeViewModel.letters.isEmpty {
-                emptyState
-            } else {
-                List {
-                    ForEach(homeViewModel.letters) { letter in
-                        NavigationLink(value: letter) {
-                            LetterRowView(letter: letter, currentUserID: currentUserID, loadedImage: homeViewModel.loadedImages[letter.id ?? ""])
-                        }
-                        .onAppear {
-                            Task {
-                                await homeViewModel.loadImageIfNeeded(for: letter)
+            Group{
+                if homeViewModel.letters.isEmpty {
+                    emptyState
+                } else {
+                    List {
+                        ForEach(homeViewModel.letters) { letter in
+                            NavigationLink(value: letter) {
+                                LetterRowView(letter: letter, currentUserID: currentUserID, loadedImage: homeViewModel.loadedImages[letter.id ?? ""])
                             }
-                        }
-                        .listRowSeparator(.hidden)
-                        .listRowInsets(
-                            EdgeInsets(
-                                top: 8,
-                                leading: 20,
-                                bottom: 8,
-                                trailing: 20
+                            .onAppear {
+                                Task {
+                                    await homeViewModel.loadImageIfNeeded(for: letter)
+                                }
+                            }
+                            .listRowSeparator(.hidden)
+                            .listRowInsets(
+                                EdgeInsets(
+                                    top: 8,
+                                    leading: 20,
+                                    bottom: 8,
+                                    trailing: 20
+                                )
                             )
-                        )
-                    }
-                    .onDelete{ indexSet in
-                        for index in indexSet {
-                            let letter = homeViewModel.letters[index]
-                            guard let id = letter.id else { continue }
-                            Task {
-                                try? await letterServices.deleteLetter(pairID: pairID, letterID: id, photoURL: letter.photoURL)
+                        }
+                        .onDelete{ indexSet in
+                            for index in indexSet {
+                                let letter = homeViewModel.letters[index]
+                                guard let id = letter.id else { continue }
+                                Task {
+                                    try? await letterServices.deleteLetter(pairID: pairID, letterID: id, photoURL: letter.photoURL)
+                                }
                             }
                         }
                     }
-                }
-                .scrollContentBackground(.hidden)
-                .background(Color(.systemGroupedBackground))
-                .toolbar{
-                    Button("New Letter", systemImage: "square.and.pencil"){
-                        isShowingNewLetter = true
+                    .scrollContentBackground(.hidden)
+                    .background(Color(.systemGroupedBackground))
+                    .toolbar{
+                        Button("New Letter", systemImage: "square.and.pencil"){
+                            isShowingNewLetter = true
+                        }
                     }
+                    .navigationTitle("Letters")
+                    .navigationBarTitleDisplayMode(.large)
                 }
-                .sheet(isPresented: $isShowingNewLetter) {
-                    if let  currentUserID {
-                        NewLetterView(existingLetter: nil, pairID: pairID, authorID: currentUserID)
-                    }
+            }
+            .sheet(isPresented: $isShowingNewLetter) {
+                if let  currentUserID {
+                    NewLetterView(existingLetter: nil, pairID: pairID, authorID: currentUserID)
                 }
-                .navigationDestination(for: Letter.self) { letter in
-                    LetterDetailView(letter: letter, pairID: pairID)
-                }
-                .navigationTitle("Letters")
-                .navigationBarTitleDisplayMode(.large)
+            }
+            .navigationDestination(for: Letter.self) { letter in
+                LetterDetailView(letter: letter, pairID: pairID)
             }
         }
     }
