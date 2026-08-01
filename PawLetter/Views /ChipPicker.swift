0b52c2012734d2a0ed33e23abcae887841705d66
Nazar Dydyn <nazardydyn83@gmail.com>
2026-08-01 21:52:16 +0300

    add gift and mood to ui

---
+++ PawLetter/Views /ChipPicker.swift
@@ -0,0 +1,18 @@
+//
+//  ChipPicker.swift
+//  PawLetter
+//
+//  Created by Nazar Dydyn on 01.08.2026.
+//
+
+import SwiftUI
+
+struct ChipPicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChipPicker()
+}
