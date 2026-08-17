66f4cad68ff9ab7e6031e3bf99112c981ca70961
Nazar Dydyn <nazardydyn83@gmail.com>
2026-08-17 18:04:29 +0300

    updating ui of LetterCardView

--- PawLetter/Home/LetterCardView.swift
+++ PawLetter/Home/LetterCardView.swift
@@ -9,18 +9,54 @@ import SwiftUI
 
 struct LetterCardView: View {
     @AppStorage("useHandwritingFont") private var useHandwritingFont: Bool = true
+    @Environment(\.dismiss) var dismiss
+    @State private var currentUserName: String?
+    @State private var userServices = UserServices()
+    
     var letter: Letter
+    var currentUserID: String?
+    var partnerName: String?
     
     var body: some View {
-        VStack {
-            Text(letter.subject)
-                .font(.handwriting(size: 28, enabled: useHandwritingFont))
+        ZStack {
+            Rectangle()
+                .fill(Color(.systemBackground))
+                .ignoresSafeArea()
             
-            if let text = letter.text{
-                Text(text)
-                    .font(.handwriting(size: 18, enabled: useHandwritingFont))
-            }
+            VStack(spacing: 32) {
+                VStack(alignment: .leading, spacing: 16) {
+                    Text(letter.subject)
+                        .font(.handwriting(size: 28, enabled: useHandwritingFont))
+                    
+                    if let text = letter.text {
+                        Text(text)
+                            .font(.handwriting(size: 18, enabled: useHandwritingFont))
+                    }
+                }
+                .frame(maxWidth: 250, minHeight: 250, alignment: .topLeading)
+                .padding(24)
+                .background(Color.accentColor.opacity(0.70))
+                .clipShape(RoundedRectangle(cornerRadius: 24))
                 
+                Button {
+                    dismiss()
+                } label: {
+                    Image(systemName: "xmark")
+                        .font(.title2)
+                        .foregroundStyle(Color.accentColor)
+                        .frame(width: 52, height: 52)
+                        .overlay(
+                            Circle()
+                                .stroke(Color.accentColor, lineWidth: 1.5)
+                        )
+                        .padding(.top, 50)
+                }
+            }
+        }
+        .task {
+            if let result = try? await userServices.fetchUserProfile(uid: currentUserID!) {
+                currentUserName = result.displayName
+            }
         }
     }
 }
@@ -30,5 +66,8 @@ struct LetterCardView: View {
         authorID: "123456789",
         subject: "We have to talk",
         text: "i love you so much",
-        createdAt: .now))
+        createdAt: .now),
+                   currentUserID: "Nazar lox",
+                   partnerName: "nastia"
+    )
 }
